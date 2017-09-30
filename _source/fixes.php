f22b0ac78f0539453a96ec157b1f8593af29a4cd
root <pgorod@users.noreply.github.com>
2017-09-30 15:45:13 +0100

    more regexp fixes: titles, cleanups, etc

--- _source/fixes.php
+++ _source/fixes.php
@@ -8,20 +8,57 @@
   $dir = new DirectoryIterator($dirname);
   foreach ($dir as $fileinfo) {
       if (!$fileinfo->isDot()) {
-           var_dump($fileinfo->getFilename());
-           $chapter = file_get_contents($fileinfo->getPathname());
-           $chapter = strtr($chapter, $array_from_to);
+          var_dump($fileinfo->getFilename());
+	  // full file replacements:
+	  $chapter = file_get_contents($fileinfo->getPathname());
+          $chapter = strtr($chapter, $array_from_to);
+     	  $chapter=preg_replace('/\[\[leanpub(.*)\]\]/', '', $chapter);
+     	  $chapter=preg_replace('/\[\[(\w-*)*chapter\]\]/', '', $chapter);
+     	  $chapter=preg_replace('/\'\'\'\'\'/', '', $chapter);
+
+          $front=preg_replace('/\n*(---\n.*\n.*\n---)(\X+)/', '$1', $chapter);
+          $rest=preg_replace('/\n*(---\n.*\n.*\n---)(\X+)/', '$2', $chapter);
+/*
+echo PHP.EOL.'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5'.PHP_EOL;
+echo $front;
+echo PHP.EOL.'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5'.PHP_EOL;
+echo substr($rest,0,300);
+echo PHP.EOL.'%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5'.PHP_EOL;
+*/
+
+       	  $rest=preg_replace('/\n(.*\n)([\~]+)/', '= $1', $rest);
+     	  $rest=preg_replace('/\n(.*\n)([\-]+)/', '== $1', $rest);
+     	  $rest=preg_replace('/\n(.*\n)([\^]+)/', '=== $1', $rest);
+ 	  $rest=preg_replace('/\n(.*\n)([\+]+)/', '==== $1', $rest);
+ 	  $chapter = $front.PHP_EOL.$rest;
+ 	  $chapter=preg_replace('/\n\n\n/', "\n\n", $chapter);
+
+	  // line by line fixes:
+          $fixed='';
+	  $count=0;
+          $lines=Array();
+          $lines=explode("\n", $chapter);
+	  echo '---------'.count($lines).'--------';	
+	  foreach ($lines as $line) {
+                 if ($line=='....') {
+                     if (!$inblock) {
+                        $inblock=true;
+                        $line='[source,php]';
+                     } else {
+                        $inblock=false;
+                        $line='';
+		     }
+                 }
+          	 $fixed = $fixed.PHP_EOL.$line;
+                 $previousLine = $line;
+          	 $count++;
+          	 //if ($count == 145) break;
+  	  }
+	  echo $count;	
            //$chapter = str_replace(
-           file_put_contents($fileinfo->getPathname(), $chapter);
-      }
+           file_put_contents($fileinfo->getPathname(), $fixed);
+      } 
+      // die();
   }
-
-die();
-
-
-
-          $pretty=str_replace($from, $to, $line);
-          $pretty=preg_replace('/\[\[(.*?)\|(.*?)\]\]/', '[$2]($1)', $pretty);
-          $pretty=preg_replace('/\[(.*?) (.*?)\]/', '[$2]($1)', $pretty);
 
 ?>
