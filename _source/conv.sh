b8f2e128025586f9e058ccd8f36c9c404f9f6357
root <pgorod@users.noreply.github.com>
2018-01-16 10:47:29 +0000

    all others

---
+++ _source/conv.sh
@@ -0,0 +1,11 @@
+pandoc -t asciidoc -f markdown _source/releases2.md > _source/releases2.adoc
+pandoc -t asciidoc -f markdown _source/releases3.md > _source/releases3.adoc
+pandoc -t asciidoc -f markdown _source/releases4.md > _source/releases4.adoc
+pandoc -t asciidoc -f markdown _source/releases5.md > _source/releases5.adoc
+pandoc -t asciidoc -f markdown _source/releases6.md > _source/releases6.adoc
+pandoc -t asciidoc -f markdown _source/releases7.md > _source/releases7.adoc
+pandoc -t asciidoc -f markdown _source/releases8.md > _source/releases8.adoc
+pandoc -t asciidoc -f markdown _source/releases9.md > _source/releases9.adoc
+pandoc -t asciidoc -f markdown _source/releases10.md > _source/releases10.adoc
+pandoc -t asciidoc -f markdown _source/releases11.md > _source/releases11.adoc
+
