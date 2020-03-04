5e43e34ba50a6afbace27c1830735405591ecc07
pgorod <pgorod@users.noreply.github.com>
2020-03-04 17:20:24 +0000

    more env vars

--- build.sh
+++ build.sh
@@ -3,11 +3,18 @@
 # output some version numbers:
 cat /etc/issue
 asciidoctor -V
+echo -------------
+echo Some Netlify environment variables you might want to know:
 echo BASE_URL is $BASE_URL
 echo BUILD_ID is $BUILD_ID
 echo CONTEXT is $CONTEXT
 echo DEPLOY_PRIME_URL is $DEPLOY_PRIME_URL
+echo URL is $URL
 echo DEPLOY_URL is $DEPLOY_URL
+echo REPOSITORY_URL is $REPOSITORY_URL
+echo BRANCH is $BRANCH
+echo COMMIT_REF is $COMMIT_REF
+echo -------------
 
 # the main call to Hugo, passing it all parameters from the command-line:
 hugo "$@"
