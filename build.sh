c2545d573ff3b8ed4267eeb668dd13193da3d148
pgorod <pgorod@users.noreply.github.com>
2020-03-04 14:03:45 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 asciidoctor -V
 hugo thou shalt fail
 hugoReturnCode = $?
-echo "Hugo return code is $hugoReturnCode"
+echo "Hugo return code is "$hugoReturnCode
 
 if [ $hugoReturnCode -ne 0 ] 
    then echo "Error is caught in if"
