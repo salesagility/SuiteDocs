d817475d90670eb2c71f2419c782d9d4de1114be
pgorod <pgorod@users.noreply.github.com>
2020-03-04 15:52:44 +0000

    comments and args

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+# output some version numbers:
 cat /etc/issue
 asciidoctor -V
-hugo 
+
+# the main call to Hugo, passing it all parameters from the command-line:
+hugo "$@"
 hugoReturnCode=$?
 
+# now the other steps in the pipeline, currently it's just htmltest, which doesn't count for the Result code
+# (it can fail or not, but it's the Hugo result code that gets signalled to Netlify in the exit)
 if [ $hugoReturnCode -ne 0 ] 
   then 
      echo "Hugo failed with return code "$hugoReturnCode"!!! ------------------------------------ !!!"
   else
      echo "Hugo succeeded, running htmltest..."
+     # they provide a nice installer of the latest version:
      curl https://htmltest.wjdp.uk | bash
+     # the tee and the rest are for counting errors by kind:
      ./bin/htmltest  | tee >(grep -v 'errors in\|failed in\|htmltest started\|======' | cut -d '-' -f 1 | sort | uniq -c -w 10)
 fi
 
