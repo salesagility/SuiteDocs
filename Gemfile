e28bc05f5d56d0108d47a1226bc36835b4c8e7b5
hybras <24651269+hybras@users.noreply.github.com>
2023-12-31 10:46:12 -0500

    Add asciidoctor server

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@
 source "https://rubygems.org"
 
 gem 'asciidoctor', '~> 2.0', '>= 2.0.17'
+gem 'asciidoctor-server', '~> 0.4.0'
--- flake.nix
+++ flake.nix
@@ -3,8 +3,13 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.asciidoctor-server = {
+    url = "github:hybras/asciidoctor-server";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, asciidoctor-server }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; }; in
         {
@@ -15,6 +20,7 @@
                 pkgs.asciidoctor-with-extensions
                 pkgs.go
                 pkgs.ruby
+                asciidoctor-server.packages.${system}.asciidoctor-client
               ];
             };
           };
